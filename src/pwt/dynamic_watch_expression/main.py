c197eb38e5bf368803655ce5bbf3180d96c218d9
PowerWordTree <PowerWordTree@Gmail.com>
2025-02-28 14:22:06 +0800

    refactor: 入口文件实现结构化日志
    完成结构化日志改造

--- src/pwt/dynamic_watch_expression/main.py
+++ src/pwt/dynamic_watch_expression/main.py
@@ -1,24 +1,51 @@
 import argparse
 import os
+import platform
 import sys
 import tempfile
 import traceback
 from types import TracebackType
-from typing import Any, Iterable
+from typing import Any, BinaryIO, Iterable
 
 import pydantic
 import tomllib
 from lark import UnexpectedInput
 
-from pwt.dynamic_watch_expression import __version__, core, log
-from pwt.dynamic_watch_expression.config import Config, Log
+from pwt.dynamic_watch_expression import (
+    __title__,
+    __version__,
+    core,
+    log,
+)
+from pwt.dynamic_watch_expression.config import Config, Log, Watcher
 from pwt.dynamic_watch_expression.constants import (
     HELP_URL,
     LOG_LEVEL_DEFAULT,
     LOG_LEVEL_OPTIONS,
     LOG_LEVEL_VERBOSE,
+    LOG_OUTPUT_OPTIONS,
     LOG_ROOT_NAME,
 )
+from pwt.dynamic_watch_expression.message import (
+    MSG_CONFIG_FILE,
+    MSG_ENTITY,
+    MSG_EPILOG,
+    MSG_ERROR,
+    MSG_ERROR_CUSTOM,
+    MSG_HELP,
+    MSG_LOG_FORMAT,
+    MSG_LOG_LEVEL,
+    MSG_LOG_LEVEL_FILTERS,
+    MSG_LOG_MSG_FILTERS,
+    MSG_LOG_NAME_FILTERS,
+    MSG_LOG_OUTPUT,
+    MSG_SIGINT,
+    MSG_STARTED,
+    MSG_TRACEBACK,
+    MSG_VERBOSE,
+    MSG_VERSION,
+)
+from pwt.utils.plugin_base import PluginInitError, PluginNotFoundError
 
 # 初始化根日志实例
 root_logger = log.get_standard_logger(LOG_ROOT_NAME)
@@ -39,13 +66,12 @@ def global_exception_handler(
     """
 
     if exc_type is KeyboardInterrupt:
-        root_logger.info("Received Ctrl+C signal, Exiting...")
+        root_logger.info(MSG_SIGINT)
     else:
         fd, path = tempfile.mkstemp(prefix="traceback-", suffix=".log", text=True)
         with os.fdopen(fd, mode="w") as file:
             traceback.print_exception(exc_type, exc_value, exc_traceback, file=file)
-        root_logger.critical(f"{exc_type.__name__}: {str(exc_value)}")
-        root_logger.critical(f"Unexpected error. Traceback log at: {path}")
+        root_logger.critical(MSG_TRACEBACK, extra=dict(path=path), exc_info=exc_value)
 
 
 def parse_args() -> argparse.Namespace:
@@ -60,7 +86,7 @@ def parse_args() -> argparse.Namespace:
         parser = _get_parser()
         return parser.parse_args()
     except argparse.ArgumentError as ex:
-        root_logger.critical(f"{type(ex).__name__}: {str(ex)}")
+        root_logger.critical(MSG_ERROR, extra=dict(exception=ex))
         raise SystemExit(2)
 
 
@@ -73,75 +99,79 @@ def _get_parser() -> argparse.ArgumentParser:
     """
 
     parser = argparse.ArgumentParser(
-        epilog=f"若需获取更详细的信息, 请访问网站: {HELP_URL}",
+        epilog=MSG_EPILOG.format(url=HELP_URL),
         add_help=False,
         exit_on_error=False,
     )
-    parser.add_argument(
-        "config_file",
-        metavar="CONFIG_FILE",
-        help="配置文件, 必要参数",
+    core_group = parser.add_argument_group("Core Parameters")
+    core_group.add_argument(
+        "config",
+        metavar="CONFIG",
+        help=MSG_CONFIG_FILE,
         type=argparse.FileType(mode="rb"),
     )
-    parser.add_argument(
-        "--log-output",
-        help="设置日志输出, 选项: std/stdout/stderr/filename.ext",
-    )
-    parser.add_argument(
-        "--log-format",
-        help="设置日志格式, 传递printf风格的格式化字符串",
-    )
-    parser.add_argument(
-        "--log-level-filters",
-        help="设置日志等级过滤, 可多次设置, 对日志等级匹配",
-        type=str.upper,
-        action="append",
-        choices=LOG_LEVEL_OPTIONS,
-    )
-    parser.add_argument(
-        "--log-name-filters",
-        help="设置日志名称过滤, 可多次设置, 对监视器名称匹配",
-        action="append",
-    )
-    parser.add_argument(
-        "--log-msg-filters",
-        help="设置日志内容过滤, 可多次设置, 对日志内容进行正则匹配",
-        action="append",
-    )
-    parser_log_level = parser.add_mutually_exclusive_group()
-    parser_log_level.add_argument(
+    log_config_group = parser.add_argument_group("Log Configuration")
+    log_config_group.add_argument(
         "--log-level",
-        help=f"设置日志输出级别, 默认: {LOG_LEVEL_DEFAULT}",
+        help=MSG_LOG_LEVEL.format(default=LOG_LEVEL_DEFAULT),
+        dest="log_level",
         type=str.upper,
         choices=LOG_LEVEL_OPTIONS,
     )
-    parser_log_level.add_argument(
+    log_config_group.add_argument(
+        "--log-output",
+        help=MSG_LOG_OUTPUT.format(options=", ".join(LOG_OUTPUT_OPTIONS)),
+    )
+    log_config_group.add_argument(
+        "--log-format",
+        help=MSG_LOG_FORMAT,
+    )
+    log_filter_group = parser.add_argument_group("Log Filtering")
+    log_filter_group.add_argument(
+        "--log-level-filters",
+        help=MSG_LOG_LEVEL_FILTERS,
+        type=str.upper,
+        action="append",
+        choices=LOG_LEVEL_OPTIONS,
+    )
+    log_filter_group.add_argument(
+        "--log-name-filters",
+        help=MSG_LOG_NAME_FILTERS,
+        action="append",
+    )
+    log_filter_group.add_argument(
+        "--log-msg-filters",
+        help=MSG_LOG_MSG_FILTERS,
+        action="append",
+    )
+    general_group = parser.add_argument_group("General Options")
+    general_group.add_argument(
         "--verbose",
         "-v",
-        help=f"显示详细信息, 等同: --log-level={LOG_LEVEL_VERBOSE}",
+        help=MSG_VERBOSE.format(level=LOG_LEVEL_VERBOSE),
         dest="log_level",
         action="store_const",
         const=LOG_LEVEL_VERBOSE,
     )
-    parser.add_argument(
+    general_group.add_argument(
         "--version",
         "-V",
-        help="显示版本号",
+        help=MSG_VERSION,
         action="version",
         version=__version__,
     )
-    parser.add_argument(
+    general_group.add_argument(
         "--help",
         "-h",
-        help="显示帮助",
+        help=MSG_HELP,
         action="help",
     )
     return parser
 
 
-def read_config_file(args: argparse.Namespace) -> dict[str, Any]:
+def read_config(file: BinaryIO) -> dict[str, Any]:
     """
-    读取并解析配置文件.
+    读取并解析配置.
 
     参数:
         args: 包含命令行参数的命名空间对象.
@@ -151,10 +181,9 @@ def read_config_file(args: argparse.Namespace) -> dict[str, Any]:
     """
 
     try:
-        with args.config_file as file:
-            return tomllib.load(file)
+        return tomllib.load(file)
     except (tomllib.TOMLDecodeError, UnicodeDecodeError) as ex:
-        root_logger.critical(f"{type(ex).__name__}: {str(ex)}")
+        root_logger.critical(MSG_ERROR, extra=dict(exception=ex))
         raise SystemExit(3)
 
 
@@ -168,19 +197,19 @@ def update_log_config(config: dict[str, Any], args: argparse.Namespace) -> None:
 
     """
 
-    log = {}
-    if args.log_output is not None:
-        log.update(output=args.log_output)
-    if args.log_format is not None:
-        log.update(format=args.log_format)
-    if args.log_level is not None:
-        log.update(level=args.log_level)
-    if args.log_level_filters is not None:
-        log.update(level_filters=args.log_level_filters)
-    if args.log_name_filters is not None:
-        log.update(name_filters=args.log_name_filters)
-    if args.log_msg_filters is not None:
-        log.update(msg_filters=args.log_msg_filters)
+    arg_mapping = {
+        "log_output": "output",
+        "log_format": "format",
+        "log_level": "level",
+        "log_level_filters": "level_filters",
+        "log_name_filters": "name_filters",
+        "log_msg_filters": "msg_filters",
+    }
+    log = {
+        config_key: getattr(args, arg_name)
+        for arg_name, config_key in arg_mapping.items()
+        if getattr(args, arg_name, None) is not None
+    }
     if log:
         config.update(logs=[log])
 
@@ -199,30 +228,37 @@ def parse_config_file(config_file: dict[str, Any]) -> Config:
     try:
         return Config.model_validate(config_file)
     except pydantic.ValidationError as ex:
-        for error in ex.errors():
-            field = ".".join(str(s) for s in error.get("loc", []))
-            msg = error.get("msg", "")
-            root_logger.critical(f"ConfigError: {field}: {msg}")
+        errors = [
+            f"{'.'.join(map(str, error['loc']))}: {error['msg']}"
+            for error in ex.errors()
+        ]
+        msg = "\n    ".join(
+            [f"{len(errors)} validation error(s) for {ex.title}:", *errors]
+        )
+        custom = dict(
+            type=type(ex).__name__,
+            message=msg,
+            traceback="".join(
+                traceback.format_exception(type(ex), ex, ex.__traceback__)
+            ),
+        )
+        root_logger.critical(MSG_ERROR_CUSTOM, extra=dict(exception=custom))
         raise SystemExit(3)
 
 
 def setup_root_logger(log_config: Iterable[Log]) -> None:
     """
-    根据日志配置设置根日志记录器的处理器。
+    根据日志配置设置根日志记录器的处理器.
 
     参数:
-        log_config (Iterable[Log]): 包含日志配置的可迭代对象。
+        log_config (Iterable[Log]): 包含日志配置的可迭代对象.
     """
 
     # 获取处理器
     try:
         handlers = log.get_handlers(log_config)
     except OSError as ex:
-        msg = f"OSError: {ex.strerror}"
-        filenames = ", ".join(f"'{s}'" for s in (ex.filename, ex.filename2) if s)
-        if filenames:
-            msg += f": {filenames}"
-        root_logger.critical(msg)
+        root_logger.critical(MSG_ERROR, extra=dict(exception=ex))
         raise SystemExit(1)
     # 清空当前处理器
     for handler in root_logger.handlers[:]:
@@ -235,18 +271,41 @@ def setup_root_logger(log_config: Iterable[Log]) -> None:
     log.autoset_level(root_logger)
 
 
-def run_core(config: Config) -> None:
+def log_startup_message(config: Config) -> None:
+    """
+    记录程序启动信息日志.
+
+    参数:
+        config: 配置对象.
+    """
+
+    python_version = (
+        f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
+    )
+    python_mode = f"{'64bit' if sys.maxsize > 2**32 else '32bit'}"
+    extra = dict(
+        program=__title__,
+        version=__version__,
+        os=f"{platform.platform()} {platform.architecture()[0]}",
+        python=f"{python_version} {python_mode}",
+        pid=os.getpid(),
+    )
+    root_logger.info(MSG_STARTED, extra=extra)
+    root_logger.debug(MSG_ENTITY, extra=dict(target=config))
+
+
+def run_core(watchers: Iterable[Watcher]) -> None:
     """
     运行核心逻辑, 启动监视器任务.
 
     参数:
-        config: 包含配置信息的Config对象.
+        watchers: 包含Watcher配置的可迭代实例.
     """
 
     try:
-        return core.start_watchers(config.watchers, root_logger)
-    except (UnexpectedInput, ModuleNotFoundError) as ex:
-        root_logger.critical(f"{type(ex).__name__}: {str(ex)}")
+        return core.start_watchers(watchers, root_logger)
+    except (UnexpectedInput, PluginNotFoundError, PluginInitError) as ex:
+        root_logger.critical(MSG_ERROR, extra=dict(exception=ex))
         raise SystemExit(3)
 
 
@@ -257,18 +316,20 @@ def main() -> int:
     args = parse_args()
     # print("\nARGS:", args, "\n")  # DEBUG
     # 读取配置文件
-    config_file = read_config_file(args)
+    with args.config as file:
+        config_file = read_config(file)
     # print("\nConfigFile:", config_file, "\n")  # DEBUG
     # 更新日志设置
     update_log_config(config_file, args)
     # print("\nConfigFile:", config_file, "\n")  # DEBUG
     # 解析配置文件
     config = parse_config_file(config_file)
-    # print("\nConfig:", config, "\n")  # DEBUG
     # 设置日志
     setup_root_logger(config.logs)
+    # 记录启动信息日志
+    log_startup_message(config)
     # 启动任务
-    run_core(config)
+    run_core(config.watchers)
     return 0
 
 
